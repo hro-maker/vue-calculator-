83b604cb9b539caf7b8e7fa944d564db185a3010
hro-maker <69071631+hro-maker@users.noreply.github.com>
2021-05-19 20:59:01 +0400

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,156 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="wraper">
+      <div class="black_board">{{ count }}</div>
+      <div class="subwraper">
+        <div v-on:click="resetnuber" class="item">C</div>
+        <div v-on:click="plyusminus" class="item">+/-</div>
+        <div v-on:click="persent" class="item">%</div>
+        <div v-on:click="divaite" class="item">/</div>
+        <div v-on:click="addnumber(7)" class="item">7</div>
+        <div v-on:click="addnumber(8)" class="item">8</div>
+        <div v-on:click="addnumber(9)" class="item">9</div>
+        <div v-on:click="time" class="item">x</div>
+        <div v-on:click="addnumber(4)" class="item">4</div>
+        <div v-on:click="addnumber(5)" class="item">5</div>
+        <div v-on:click="addnumber(6)" class="item">6</div>
+        <div v-on:click="minus" class="item">-</div>
+        <div v-on:click="addnumber(1)" class="item">1</div>
+        <div v-on:click="addnumber(2)" class="item">2</div>
+        <div v-on:click="addnumber(3)" class="item">3</div>
+        <div v-on:click="pluse" class="item">+</div>
+        <div v-on:click="addnumber(0)" class="item big_item">0</div>
+        <div v-on:click="dote" class="item">.</div>
+        <div v-on:click="equal" class="item">=</div>
+      </div>
+      <div class="condition" v-if="prevvale" >{{prevvale}} {{oper}} ?</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      count: "0",
+      operator: null,
+      prevvale: null,
+      oper: null,
+    };
+  },
+  methods: {
+    addnumber(num) {
+      if (this.count == "0") {
+        this.count = num.toString();
+      } else {
+        this.count += num;
+      }
+    },
+    resetnuber() {
+      this.count = "0";
+      this.operator = null;
+      this.prevvale = null;
+    },
+    plyusminus() {
+      if (this.count !== "0") {
+        this.count = `${this.count * -1}`;
+      }
+    },
+    persent() {
+      this.count = `${parseFloat(this.count) / 100}`;
+    },
+    dote() {
+      if (!this.count.includes(".")) {
+        this.count += ".";
+      }
+    },
+    pluse() {
+      if (this.count !== "0") {
+        this.operator = (a, b) => parseFloat(a) + parseFloat(b);
+        this.prevvale = this.count;
+        this.count = "0";
+        this.oper = "+";
+      }
+    },
+    minus() {
+      if (this.count !== "0") {
+        this.operator = (a, b) => parseFloat(a) - parseFloat(b);
+        this.prevvale = this.count;
+        this.count = "0";
+        this.oper = "-";
+      }
+    },
+    time() {
+      if (this.count !== "0") {
+        this.operator = (a, b) => parseFloat(a) * parseFloat(b);
+        this.prevvale = this.count;
+        this.count = "0";
+        this.oper = "*";
+      }
+    },
+    divaite() {
+      if (this.count !== "0") {
+        this.operator = (a, b) => parseFloat(a) / parseFloat(b);
+        this.prevvale = this.count;
+        this.count = "0";
+        this.oper = "/";
+      }
+    },
+    equal() {
+      if (this.prevvale) {
+        this.count = `${this.operator(this.prevvale, this.count)}`;
+        this.prevvale = null;
+        this.operator = null;
+        this.oper = null;
+      } else {
+        this.count = "0";
+        this.prevvale = null;
+        this.operator = null;
+        this.oper = null;
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+
+<style scoped lang="scss">
+.wraper {
+  width: 30%;
+  min-height: 40%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.black_board {
+  width: 100%;
+  height: 30px;
+  font-size: 40px;
+  padding: 10px 0;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.subwraper {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: auto;
 }
-a {
-  color: #42b983;
+.item {
+  background-color: rgb(236, 78, 15);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  padding: 5px;
+  margin: 2px;
+  cursor: pointer;
+}
+.big_item {
+  grid-column: 1 / 3;
+}
+.condition {
+  color: black;
+  font-size: 30px;
 }
 </style>
